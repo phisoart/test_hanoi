ae5e9bd1b0792086a23c20487214b9b252b5ab9c
royjang12 <royjang12@gmail.com>
2022-10-18 10:19:25 +0900

    start

---
+++ main.py
@@ -0,0 +1,19 @@
+
+def test():
+    return True
+
+def hanoi(_height, _algorithm):
+    make input...
+    print(input)
+    output, comp_n = algorithm (input)
+    print(output)
+    return comp_n
+if __name__ == '__main__':
+    height = 10
+
+    if ~test(Algorithm ###):
+        print ('test fail')
+
+    comp_n = hanoi(height, algorithm)
+
+    print(height, comp_n)
