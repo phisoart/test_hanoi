a7cd604ba6e57d5059cbb073994e7fe7847b40cf
royjang12 <royjang12@gmail.com>
2022-10-18 10:54:39 +0900

    create hanoi_init

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+import numpy as np
 
 def test():
     return True
 
+def hanoi_init(_height):
+    return np.arange(1, _height+1), np.zeros(_height), np.zeros(_height)
+
 def hanoi(_height, _algorithm):
-    make input...
-    print(input)
-    output, comp_n = algorithm (input)
-    print(output)
-    return comp_n
+    source, auxiliary, destination = hanoi_init(_height)
+    # print(input)
+    # output, comp_n = algorithm (input)
+    # print(output)
+    # return comp_n
+
 if __name__ == '__main__':
     height = 10
 
-    if ~test(Algorithm ###):
-        print ('test fail')
+    # if ~test(Algorithm ###):
+    #     print ('test fail')
 
-    comp_n = hanoi(height, algorithm)
+    comp_n = hanoi(height, "")
 
-    print(height, comp_n)
+    # print(height, comp_n)
